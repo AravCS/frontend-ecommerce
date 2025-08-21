e4ff409378608c8df9a9a48cc2c421ee639eaa65
AravShah1 <ashah69@student.ohlone.edu>
2025-08-20 22:27:57 -0700

    added some stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
 import { useEffect, useState } from "react";
-import Homepage from "./components/Homepage";
+import "./index.css";
+import Homepage from "./components/Homepage.jsx";
 
 export default function App() {
-    const [products, setProducts] = useState([]);
+    const [mockItems, setMockItems] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
+    // fetch mock items from FakeStore API on component mount
     useEffect(() => {
         fetch("https://fakestoreapi.com/products")
-            .then((res) => res.json())
-            .then((data) => setProducts(data))
-            .catch((err) => console.error(err));
+            .then((res) => {
+                if (res.status >= 400) {
+                    throw new Error("server error");
+                }
+                return res.json();
+            })
+            .then((data) => {
+                setMockItems(data);
+            })
+            .catch((err) => setError(err))
+            .finally(() => setLoading(false));
     }, []);
 
-    return <Homepage products={products} />;
+    // if fetching is not ready
+    if (loading) return <p>Loading Website ... </p>;
+    // if error occurs
+    if (error) return <p>An error happen :( . . . </p>;
+
+    return (
+        <>
+           <Homepage mockItems={mockItems} />
+        </>
+    );
 }
